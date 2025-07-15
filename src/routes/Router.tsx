03b4b28b2da4e5e79335229b0800f5160689beac
ShailySarker <shailysarker99@gmail.com>
2025-07-16 00:01:26 +0600

    not found page added

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { createBrowserRouter } from "react-router";
 import LazyLoader from "../components/LazyLoader";
 const LandingPage = React.lazy(() => import("../pages/LandingPage/LandingPage"));
+const NotFound = React.lazy(() => import("../pages/NotFound/NotFound"));
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +11,13 @@ export const router = createBrowserRouter([
         element:
             <Suspense fallback={<LazyLoader />}>
                 <LandingPage />
+            </Suspense>
+    },
+    {
+        path: "/*",
+        element:
+            <Suspense fallback={<LazyLoader />}>
+                <NotFound />
             </Suspense>
     },
 ])
