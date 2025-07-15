111798ee587b6190092b42bd64f95e332dd96933
ShailySarker <shailysarker99@gmail.com>
2025-07-16 00:05:32 +0600

    navbar and footer added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
         <>
-           <Outlet/> 
+            <Navbar/>
+                <Outlet/>
+            <Footer />
         </>
     );
 };
